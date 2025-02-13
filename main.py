1b7e5138c636e1802ca51ad4bc2aab74d9206cf6
IDGS-801-22000823 <gutierrez.ascencio.luis@gmail.com>
2025-02-12 23:30:44 -0600

    12-02-2025_Cinepolis_html_python

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
@@ -53,6 +53,78 @@ def aperas():
     </form>
     '''
     
+@app.route("/operas")
+def operas():
+    return render_template("OperasBas.html")
+
+@app.route("/resultado", methods=["GET","POST"])
+def result():
+    n1=request.form["n1"]
+    n2=request.form["n2"]
+    return "La multiplicacion de {} x {} es {}".format(n1,n2,str(int(n1)*int(n2)))
+
+@app.route("/operaciones", methods=["GET", "POST"])
+def operaciones():
+    resultado = ""
+    if request.method == "POST":
+        n1 = float(request.form.get("n1"))
+        n2 = float(request.form.get("n2"))
+        operacion = request.form.get("operacion")
+        if operacion == "suma":
+            resultado = n1 + n2
+        elif operacion == "resta":
+            resultado = n1 - n2
+        elif operacion == "multiplicacion":
+            resultado = n1 * n2
+        elif operacion == "division":
+            if n2 != 0:
+                resultado = n1 / n2
+            else:
+                resultado = "Error"
+        else:
+            resultado = "Nop"
+    return render_template("OperasBas.html", resultado=resultado)
+
+@app.route("/cinepolis", methods=["GET", "POST"])
+def cinepolis():
+    resultado = ""
+    if request.method == "POST":
+        try:
+            nombre = request.form["nombre"]
+            compra = int(request.form["compra"])
+            tarjeta = request.form["tarjeta"]
+            boletos = int(request.form["boletos"])
+            if compra <= 0 or boletos <= 0:
+                resultado = "Como que 0 o menos ._.?"
+            else:
+                limite_boletos = compra * 7
+                
+                if boletos > limite_boletos:
+                    resultado = f"No se pueden comprar mas de {limite_boletos} boletos para {compra} personas."
+                else:
+                    # Si hay descuento ZIP si no NOP
+                    if boletos > 5:
+                        descuento = 0.15
+                    elif boletos >= 3:
+                        descuento = 0.10
+                    else:
+                        descuento = 0
+
+                    # tienes tarjeta vienestar de morena sjjsjsjs
+                    if tarjeta == "si":
+                        descuento += 0.10  # 10% adicional
+                    # total
+                    precio_boleto = 12
+                    total_sin_descuento = boletos * precio_boleto
+                    total_con_descuento = total_sin_descuento * (1 - descuento)
+
+                    resultado = f"${total_con_descuento:.2f}"
+        except ValueError:
+            resultado = "Pero pon numeros."
+
+    return render_template("cinepolis.html", resultado=resultado)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
